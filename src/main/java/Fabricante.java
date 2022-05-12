d578aaa9407ff87e107cafbec8fe82b8413d5b1b
nicolasgalfione <nicogalfione@gmail.com>
2022-05-12 13:26:45 -0300

    agregamos idea

--- src/main/java/Fabricante.java
+++ src/main/java/Fabricante.java
@@ -13,3 +13,9 @@ class Fabricante {
   }
 
 }
+
+
+//el fabricante es un sastre enrealidad que puede: fabricarUniforme(recibe instituto) y sugerirAtuendos(recibe un guardarropas)
+//siendo que ademas los unifiormes y atuendos son muy parecidos
+//a su vez el uniforme es un tipo de atuendo
+//basicamente agregamos al factory de uniforme to.do lo de atuendos para el usuario
